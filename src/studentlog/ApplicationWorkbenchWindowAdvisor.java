f3999e414b04285d5bc8be3a3bb258fe1fa52356
H215051 <H215051@MMUNTIAN.rdx.lgc.com>
2018-05-24 12:42:52 +0300

    Refactoring
    Got target for DND

--- src/studentlog/ApplicationWorkbenchWindowAdvisor.java
+++ src/studentlog/ApplicationWorkbenchWindowAdvisor.java
@@ -1,15 +1,20 @@
 package studentlog;
 
+import org.eclipse.swt.dnd.DND;
 import org.eclipse.swt.dnd.DropTargetAdapter;
 import org.eclipse.swt.dnd.DropTargetEvent;
+import org.eclipse.swt.dnd.DropTargetListener;
 import org.eclipse.swt.dnd.TextTransfer;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+import org.eclipse.ui.part.EditorInputTransfer;
 
 import com.google.gson.Gson;
 
@@ -38,21 +43,84 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 		configurer.setShowStatusLine(true);
 		configurer.setTitle("StudentLog");
 		
-		IWorkbenchPage page = configurer.getWindow().getActivePage();
-		configurer.addEditorAreaTransfer(TextTransfer.getInstance());
-		configurer.configureEditorAreaDropListener(new DropTargetAdapter() {
-			public void drop(DropTargetEvent event) {
-				StudentsEntry entry = new Gson().fromJson((String) event.data, StudentsEntry.class);
-				StudentProfileEditorInput input = new StudentProfileEditorInput(entry.getName());
-				StudentProfileEditor newEditor;
-				try {
-					newEditor = (StudentProfileEditor) page.openEditor(input, StudentProfileEditor.ID);
-					newEditor.getPanel().fillPanelArea(entry);
-				} catch (PartInitException e) {
-					e.printStackTrace();
-				}
-			}
-		});
+				configurer.addEditorAreaTransfer(EditorInputTransfer.getInstance());
+				configurer.configureEditorAreaDropListener(new DropTargetListener() {
+					
+					private IWorkbenchWindow window = configurer.getWindow();
+		
+		//			public DropTargetListener(IWorkbenchWindow window) {
+		//				this.window = window;
+		//			}
+		
+					@Override
+					public void dragEnter(DropTargetEvent event) {
+						event.detail = DND.DROP_COPY;
+						event.feedback = DND.FEEDBACK_SELECT;
+					}
+		
+					@Override
+					public void dragLeave(DropTargetEvent event) {
+					}
+		
+					@Override
+					public void dragOperationChanged(DropTargetEvent event) {
+						event.detail = DND.DROP_COPY;
+						event.feedback = DND.FEEDBACK_NONE;
+					}
+		
+					@Override
+					public void dragOver(DropTargetEvent event) {
+						event.detail = DND.DROP_COPY;
+						event.feedback = DND.FEEDBACK_SELECT;
+					}
+		
+					public void drop(DropTargetEvent event) {
+						IWorkbenchPage page = window.getActivePage();
+						System.out.println("----Drop-----");
+						System.out.println(event.data);
+						EditorInputTransfer.EditorInputData[] editorInputs = (EditorInputTransfer.EditorInputData[]) event.data;
+
+						for (int i = 0; i < editorInputs.length; i++) {
+							IEditorInput input = editorInputs[i].input;
+							try {
+								page.openEditor(input, StudentProfileEditor.ID);
+							} catch (PartInitException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+
+//		 				StudentsEntry entry = new Gson().fromJson((String) event.data, StudentsEntry.class);
+//						StudentProfileEditorInput input = new StudentProfileEditorInput(entry.getName());
+//		
+//		 					event.printStackTrace();
+		 				}
+
+		
+					@Override
+					public void dropAccept(DropTargetEvent event) {
+						// TODO Auto-generated method stub
+						
+					}
+				});
+		 //		DropTarget dt = new DropTarget(co, DND.DROP_MOVE);
+		 //		dt.setTransfer(new Transfer[] { TextTransfer.getInstance() });
+		
+//		IWorkbenchPage page = configurer.getWindow().getActivePage();
+//		configurer.addEditorAreaTransfer(TextTransfer.getInstance());
+//		configurer.configureEditorAreaDropListener(new DropTargetAdapter() {
+//			public void drop(DropTargetEvent event) {
+//				StudentsEntry entry = new Gson().fromJson((String) event.data, StudentsEntry.class);
+//				StudentProfileEditorInput input = new StudentProfileEditorInput(entry.getName());
+//				StudentProfileEditor newEditor;
+//				try {
+//					newEditor = (StudentProfileEditor) page.openEditor(input, StudentProfileEditor.ID);
+//					newEditor.getPanel().fillPanelArea(entry);
+//				} catch (PartInitException e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
 //		DropTarget dt = new DropTarget(co, DND.DROP_MOVE);
 //		dt.setTransfer(new Transfer[] { TextTransfer.getInstance() });
 //		dt.addDropListener(new DropTargetAdapter() {
--- src/studentlog/views/StudentsView.java
+++ src/studentlog/views/StudentsView.java
@@ -1,5 +1,7 @@
 package studentlog.views;
 
+import java.util.Iterator;
+
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
@@ -7,13 +9,20 @@ import org.eclipse.swt.dnd.DND;
 import org.eclipse.swt.dnd.DragSource;
 import org.eclipse.swt.dnd.DragSourceAdapter;
 import org.eclipse.swt.dnd.DragSourceEvent;
+import org.eclipse.swt.dnd.DragSourceListener;
 import org.eclipse.swt.dnd.TextTransfer;
 import org.eclipse.swt.dnd.Transfer;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.TreeItem;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.part.EditorInputTransfer;
 import org.eclipse.ui.part.ViewPart;
 
 import com.google.gson.Gson;
 
+import studentlog.editors.StudentProfileEditor;
+import studentlog.editors.StudentProfileEditorInput;
+import studentlog.model.ITreeItem;
 import studentlog.model.Observer;
 import studentlog.model.Root;
 import studentlog.model.StudentsEntry;
@@ -42,35 +51,159 @@ public class StudentsView extends ViewPart implements Observer {
 		treeViewer.setLabelProvider(new CustomTreeLabelProvider());
 		root = TreeModel.getInstance().getRoot();
 		treeViewer.setInput(root);
-		
-		DragSource ds = new DragSource(treeViewer.getTree(), DND.DROP_MOVE);
 
-		ds.setTransfer(new Transfer[] {TextTransfer.getInstance()});
-		ds.addDragListener(new DragSourceAdapter() {
+		int operations = DND.DROP_COPY | DND.DROP_MOVE;
+		Transfer[] transferTypes = new Transfer[] { EditorInputTransfer.getInstance() };
+		treeViewer.addDragSupport(operations, transferTypes, new DragSourceListener() {
+
+			@Override
+			public void dragStart(DragSourceEvent event) {
+				
+			}
+
+			@Override
 			public void dragSetData(DragSourceEvent event) {
-				IStructuredSelection selection = treeViewer.getStructuredSelection();
-				StudentsEntry firstElement = (StudentsEntry)selection.getFirstElement();
-				Gson gson = new Gson();
-				String jsonStr = gson.toJson(firstElement);
-		        event.data = jsonStr;
-		      }
-		    });
-		
-		
+				if (EditorInputTransfer.getInstance().isSupportedType(event.dataType)) { // if
+					// support
+					// EditorInputTransfer
+					IStructuredSelection selection = treeViewer.getStructuredSelection();
+					int i = 0;
+					EditorInputTransfer.EditorInputData[] arrData = new EditorInputTransfer.EditorInputData[selection
+							.size()];
+					Iterator<?> iterator = selection.iterator();
+					while (iterator.hasNext()) {
+						StudentsEntry node = (StudentsEntry) iterator.next();
+						if (node instanceof ITreeItem) {
+							IEditorInput input = new StudentProfileEditorInput(((StudentsEntry) node).getName());
+							arrData[i] = EditorInputTransfer.createEditorInputData(StudentProfileEditor.ID, input);
+							i++;
+						}
+					}
+					event.data = arrData;
+				}// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void dragFinished(DragSourceEvent event) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+
+		// DragSource ds = new DragSource(treeViewer.getTree(), DND.DROP_MOVE);
+		// // DragSource ds = new DragSource(treeViewer.getTree(),
+		// DND.DROP_MOVE);
+		//
+		// ds.setTransfer(new Transfer[] {TextTransfer.getInstance()});
+		// ds.addDragListener(new DragSourceAdapter() {
+		// public void dragSetData(DragSourceEvent event) {
+		// IStructuredSelection selection = treeViewer.getStructuredSelection();
+		// StudentsEntry firstElement =
+		// (StudentsEntry)selection.getFirstElement();
+		// Gson gson = new Gson();
+		// String jsonStr = gson.toJson(firstElement);
+		// event.data = jsonStr;
+		// }
+		// });
+		// int operations = DND.DROP_COPY | DND.DROP_MOVE ;
+		// Transfer[] transferTypes = new Transfer[] {
+		// EditorInputTransfer.getInstance() };
+		// // ds.setTransfer(transferTypes);
+		// // ds.setTransfer(new Transfer[] {TextTransfer.getInstance()});
+		// treeViewer.addDragSupport(operations, transferTypes, new
+		// NodeDragListener(treeViewer));
+		// // ds.addDragListener(new DragSourceAdapter() {
+		// // public void dragSetData(DragSourceEvent event) {
+		// // IStructuredSelection selection =
+		// treeViewer.getStructuredSelection();
+		// // StudentsEntry firstElement =
+		// (StudentsEntry)selection.getFirstElement();
+		// // Gson gson = new Gson();
+		// // String jsonStr = gson.toJson(firstElement);
+		// // event.data = jsonStr;
+		// //
+		// //// IStructuredSelection selection =
+		// viewer.getStructuredSelection();
+		// // int i=0;
+		// // EditorInputTransfer.EditorInputData[] arrData=new
+		// EditorInputTransfer.EditorInputData[selection.size()];
+		// // Iterator<?> iterator=selection.iterator();
+		// // while(iterator.hasNext()){
+		// // TreeItem node=(TreeItem)iterator.next();
+		// // if (node instanceof ITreeItem) {
+		// // IEditorInput input = new StudentProfileEditorInput(((ITreeItem)
+		// node).getName());
+		// //
+		// arrData[i]=EditorInputTransfer.createEditorInputData(StudentProfileEditor.ID,
+		// input);
+		// // i;
+		// // }
+		// // }
+		// // event.data=arrData;
+		// // }
+		// // });
+		//
+		//
 	}
 
-	@Override
-	public void setFocus() {
-		treeViewer.getControl().setFocus();
+	public TreeViewer getTreeViewer() {
+		return treeViewer;
+	}
+
+	private class NodeDragListener implements DragSourceListener {
+
+		private final TreeViewer viewer;
+
+		public NodeDragListener(TreeViewer viewer) {
+			this.viewer = viewer;
+		}
+
+		@Override
+		public void dragStart(DragSourceEvent event) {
+			// TODO Autogenerated method stub
+			System.out.println("Start Drag");
+		}
+
+		@Override
+		public void dragSetData(DragSourceEvent event) {
+			if (EditorInputTransfer.getInstance().isSupportedType(event.dataType)) { // if
+																						// support
+																						// EditorInputTransfer
+				IStructuredSelection selection = viewer.getStructuredSelection();
+				int i = 0;
+				EditorInputTransfer.EditorInputData[] arrData = new EditorInputTransfer.EditorInputData[selection
+						.size()];
+				Iterator<?> iterator = selection.iterator();
+				while (iterator.hasNext()) {
+					TreeItem node = (TreeItem) iterator.next();
+					if (node instanceof ITreeItem) {
+						IEditorInput input = new StudentProfileEditorInput(((ITreeItem) node).getName());
+						arrData[i] = EditorInputTransfer.createEditorInputData(StudentProfileEditor.ID, input);
+						i++;
+					}
+				}
+				event.data = arrData;
+			}
+		}
+
+		@Override
+		public void dragFinished(DragSourceEvent event) {
+			if (event.detail == DND.DROP_MOVE) {
+				System.out.println("Finished Drag");
+			}
+		}
 	}
 
 	@Override
 	public void update(Root root) {
-		treeViewer.setInput(root);
+		// TODO Auto-generated method stub
 
 	}
-	
-	public TreeViewer getTreeViewer() {
-		return treeViewer;
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
 	}
 }
